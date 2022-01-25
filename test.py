1c0c5e3d9e92dd7f29601682946159857f10c973
Haryanto <M07158031@o365.mcut.edu.tw>
2022-01-25 11:56:54 +0800

    fix bug

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import cv2
 
 image = cv2.imread("image.jpg")
 moildev = Moildev("Raspi_Cam.json")
-alpha, beta = moildev.get_alpha_beta(900, 0, 1)
+alpha, beta = moildev.getAlphaBeta(900, 0, 1)
 image = moildev.anypoint(image, alpha,beta,4, 1)
 
 resized_image = cv2.resize(image, (800, 600), interpolation=cv2.INTER_AREA)
